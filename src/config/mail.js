d73bf70d671bd6a0911da8a79351ed4bc85d0b2b
Matheus Allan <matheusrpd@gmail.com>
2020-01-17 12:44:25 -0300

    Add cancelamento de agendamento e envio de email.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd4857edbee0023',
+    pass: '15fa056279faa7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
